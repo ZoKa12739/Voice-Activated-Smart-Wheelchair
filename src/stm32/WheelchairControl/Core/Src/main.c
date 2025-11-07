c499f9f04a8873b48b6798372ccfd294cb077e80
ZoKa12739 <zky12739@gmail.com>
2025-11-07 21:21:51 +0800

    删去废弃代码
    换为最新：实现原控制逻辑

---
+++ src/stm32/WheelchairControl/Core/Src/main.c
@@ -0,0 +1,244 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "dac.h"
+#include "dma.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>  // 用于 sprintf
+#include <string.h> // 用于 strlen
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+// "volatile" 关键字告诉编译器，这个变量可能会在任何时候被外部（即DMA）改变
+volatile uint16_t adc_values[2];// 定义一个16位无符号整数数组，大小为 2，用于存储 X 和 Y 轴的 ADC 结果
+// 用于存放要发送的串口字符串
+char uart_buf[100];
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_ADC1_Init();
+  MX_DAC_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  // 启动 ADC1 并开启 DMA 模式，数据存入 adc_values 数组
+HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_values, 2);
+  // // 启动 DAC 通道 1 (PA4)
+  HAL_DAC_Start(&hdac, DAC_CHANNEL_1);
+  // // 启动 DAC 通道 2 (PA5)
+  HAL_DAC_Start(&hdac, DAC_CHANNEL_2);
+
+  // 启动 ADC1 并开启 DMA 模式
+  // &hadc1: ADC 句柄
+  // (uint32_t*)adc_values: DMA 目标地址 (这里做一下强制类型转换)
+  // 2: 要转换的数据个数
+  HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_values, 2);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+    /*
+    * 此时，DMA 正在后台自动、连续地：
+    * 1. 转换 PA0 (X轴) -> 存入 adc_values[0]
+    * 2. 转换 PA1 (Y轴) -> 存入 adc_values[1]
+    * 3. 循环往复...
+    */
+
+    // // 1. 将 X 轴的 ADC 值 (0-4095) 写入 DAC 通道 1 (PA4)
+    // //    adc_values[0] 对应 Rank 1 (PA0)
+     HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, adc_values[0]);
+
+    // // 2. 将 Y 轴的 ADC 值 (0-4095) 写入 DAC 通道 2 (PA5)
+    // //    adc_values[1] 对应 Rank 2 (PA1)
+     HAL_DAC_SetValue(&hdac, DAC_CHANNEL_2, DAC_ALIGN_12B_R, adc_values[1]);
+
+    // // (可选) 加一个 5ms 的小延迟，让 CPU 稍微休息
+     HAL_Delay(5);
+
+    
+    // 1. 将 ADC 原始值 (0-4095) 格式化为字符串
+    //    %u 表示 "unsigned int" (无符号整数)
+    //    \r\n 表示回车换行，这样串口助手里能自动换行
+    int len = sprintf(uart_buf, "X-Axis: %u,  Y-Axis: %u\r\n", adc_values[0], adc_values[1]);
+
+    // 2. 通过 USART2 发送格式化后的字符串
+    //    &huart2 是你的 USART2 句柄 (CubeMX 自动生成的)
+    //    (uint8_t*)uart_buf 是字符串数据
+    //    len 是字符串长度
+    //    100 是超时时间 (ms)，如果 100ms 内发不出去就放弃
+    HAL_UART_Transmit(&huart2, (uint8_t*)uart_buf, len, 100);
+
+    // 3. 延迟一段时间，比如 100ms
+    //    如果不延迟，你的串口会被海量数据刷屏，电脑会卡住
+    HAL_Delay(100);
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
